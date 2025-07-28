1363e8661aa22853de52a0ffd545f654f40e23fd
Maxx730 <max.kinghorn@gmail.com>
2025-07-28 19:59:06 -0400

    added viewers

---
+++ video-stream-viewers/src/index.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+interface Viewer {
+    ip: string,
+    lastPing: Date
+}
+
+interface ViewerCount {
+    viewerCount: number
+}
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 2276;
+const pruneInterval = 5000;
+const pruneTimeoutMinutes = 5.0;
+let connected: Array<Viewer> = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: '*' }));
+app.get('/viewers', (req: Request<{}, {}, ViewerCount>, res: Response) => {
+    const ip: string = req.ip || '';
+    const findVal = (viewer: Viewer) => viewer.ip === ip;
+    if (connected.find(findVal)) {
+        const found = connected[connected.findIndex(findVal)];
+        found.lastPing = new Date();
+    } else {
+        connected.push({
+            ip,
+            lastPing: new Date()
+        });
+    }
+    res.json({
+        viewerCount: connected.length
+    });
+});
+setInterval(() => {
+    connected = connected.filter((viewer: Viewer) => {
+        const sinceLast = new Date().getTime() - viewer.lastPing.getTime();
+        const seconds = sinceLast / 1000;
+        const minutes = seconds / 60;
+        return minutes < pruneTimeoutMinutes;
+    });
+}, pruneInterval);
+app.listen(port, () => {});
