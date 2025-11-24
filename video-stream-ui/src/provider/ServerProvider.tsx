299fdbd14a464a91a12597d9a0602c750618e7bd
Maxx730 <max.kinghorn@gmail.com>
2025-11-23 22:00:11 -0500

    updated nginx

--- video-stream-ui/src/provider/ServerProvider.tsx
+++ video-stream-ui/src/provider/ServerProvider.tsx
@@ -77,7 +77,7 @@ export const ServerProvider: React.FC<{
 
     // VIEWERS
     const getViewers = async () => {
-        const viewersResponse = await fetch(`https://${serverIp}:2277/viewers/`);
+        const viewersResponse = await fetch(`https://${serverIp}/viewers`);
         if (!viewersResponse.ok) {
             addError('Error loading viewers...');
         }
@@ -85,7 +85,7 @@ export const ServerProvider: React.FC<{
         return data;
     }
     const getPing = async () => {
-        const pingResponse = await fetch(`https://${serverIp}:2277/ping`);
+        const pingResponse = await fetch(`https://${serverIp}/ping`);
         if (!pingResponse.ok) {
             addError(`ERROR: Error pinging...`);
         }
@@ -101,7 +101,7 @@ export const ServerProvider: React.FC<{
 
     // ACTIONS
     const join = async (key: string) => {
-            const joinResponse = await fetch(`https://${serverIp}:2277/join`, {
+            const joinResponse = await fetch(`https://${serverIp}/join`, {
                 method: 'POST',
                 headers: JSON_HEADERS,
                 body: JSON.stringify({ channel: key })
@@ -115,7 +115,7 @@ export const ServerProvider: React.FC<{
             }
     }
     const watch = async (key: string) => {
-        const watchResponse = await fetch(`https://${serverIp}:2277/watch`, {
+        const watchResponse = await fetch(`https://${serverIp}/watch`, {
             method: 'POST',
             headers: JSON_HEADERS,
             body: JSON.stringify({ channel: key })
